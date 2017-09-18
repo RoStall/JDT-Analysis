b1a7d2d4ab6ee0838f1efc25210fb9d1e4adc491
Robert Stallard <robertwstallard@gmail.com>
2017-09-18 18:17:19 -0400

    Filling in prep work

---
+++ JDT_SVM_binary.r
@@ -0,0 +1,37 @@
+#SVM for JDT. Post LASSO. 
+library(tibble)
+library(e1071)
+library(dplyr)
+library(plyr) # load order with above might matter
+setwd('~/dropbox/nasa_stretch/force_features')
+data = read.csv('~/dropbox/nasa_stretch/force_features/force_emg_expl.csv')
+
+# calculate the bilateral ratios (4 total)
+data$bmg_wav =(data$lmg_airsum + data$rmg_airsum)/(data$lmg_lsrsum + data$rmg_lsrsum)
+data$bmg_iemg =(data$lmg_iemg_air + data$rmg_iemg_air)/(data$lmg_iemg_lnd + data$rmg_iemg_lnd)
+data$bta_wav =(data$lta_airsum +data$rta_airsum)/(data$lta_lsrsum+ data$rta_lsrsum)
+data$bta_iemg = (data$lta_iemg_air + data$rta_iemg_air)/(data$lta_iemg_lnd + data$rta_iemg_lnd)
+
+data = data[,c(1:4,5:20, 31:34, 21:30)]
+
+sep_plat = dlply(data, "Platform", identity)
+
+df.control = sep_plat[[1]]
+df.flywheel = sep_plat[[2]]
+df.iss = sep_plat[[3]]
+df.shuttle = sep_plat[[4]]
+df.treatmenta = sep_plat[[5]]
+df.treatmentb = sep_plat[[6]]
+
+# lets examine only ISS case for now.
+
+# Split ISS cases
+day_split_iss = dlply(df.iss,"normTime", identity)
+# 1-A; 2-B; 3-C; 4-E; 5-F; 6-G
+
+#BC vs. E
+
+#E vs. FG
+
+#E vs. G
+
